0c8343be33746b0b236b7708e6815b2f6df1d391
Moath <moath.alhajrii@gmail.com>
2021-07-03 18:36:10 +0300

    add test #1

---
+++ src/index.test.js
@@ -0,0 +1,11 @@
+const convert = require('./index');
+
+test('it can convert arabic numbers to english numbers', () => {
+    expect(convert('test ١٢٣')).toBe('test 123');
+    expect(convert('كلمة ١٢٣')).toBe('كلمة 123');
+    expect(convert('٠١٢٣٤٥٦٧٨٩')).toBe('0123456789');
+
+    const object = {};
+
+    expect(convert(object)).toBe(object);
+});
